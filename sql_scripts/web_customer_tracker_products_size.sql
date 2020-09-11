cb3b0c67168851b1e8df3eb67cb8958c257e503b
DuongCa0 <47597043+DuongCa0@users.noreply.github.com>
2020-09-11 17:34:58 +0700

    table customer,order,feedback

--- sql_scripts/web_customer_tracker_products_size.sql
+++ sql_scripts/web_customer_tracker_products_size.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `products_size`
 --
-
+use project4;
 DROP TABLE IF EXISTS `products_size`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
--- src/main/java/vn/aptech/project4/controller/LoginControler.java
+++ src/main/java/vn/aptech/project4/controller/LoginControler.java
@@ -14,5 +14,6 @@ public class LoginControler {
 	@GetMapping("/access-denied")
 	public String showAccessDenied() {
 		return "utility-access-denied";
+				
 	}
 }
