49e45ad9f405111588a373641db95b105996b019
dinhkien <nguyendinhkien2605@gmail.com>
2020-10-20 17:16:27 +0700

    kien test commikien test commikien test committ

--- src/main/java/vn/aptech/project4/entity/Customer.java
+++ src/main/java/vn/aptech/project4/entity/Customer.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-package vn.aptech.project4.entity;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name="customer")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Customer {
-	@Id
-	@Column(name = "customer_id")
-	private int customer_id;
-	@Column(name = "customer_email")
-	private String customerEmail;
-	@Column(name = "customer_password")
-	private String customer_password;
-	@Column(name = "customer_name")
-	private String customer_name;
-	@Column(name = "customer_phone")
-	private String customer_phone;
-	@Column(name = "address")
-	private String address;
-	@Column(name = "total_expense")
-	private int total_expense;
-	@ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
-	@JoinColumn(name="membership_id")
-	private Membership membership;
-	@Column(name="authority")
-	private String authority;
-	
-}
-=======
 package vn.aptech.project4.entity;
 
 import javax.persistence.*;
@@ -60,7 +15,7 @@ public class Customer {
 	@Column(name = "customer_name")
 	private String customer_name;
 	@Column(name = "customer_phone")
-	private int customer_phone;
+	private String customer_phone;
 	@Column(name = "address")
 	private String address;
 	@Column(name = "total_expense")
@@ -106,11 +61,11 @@ public class Customer {
 		this.customer_name = customer_name;
 	}
 
-	public int getCustomer_phone() {
+	public String getCustomer_phone() {
 		return customer_phone;
 	}
 
-	public void setCustomer_phone(int customer_phone) {
+	public void setCustomer_phone(String customer_phone) {
 		this.customer_phone = customer_phone;
 	}
 
@@ -146,4 +101,4 @@ public class Customer {
 		this.authority = authority;
 	}
 }
->>>>>>> upstream/master
+
